77dbc3bed0f9cf2d5efcf702c76182eb83dda17b
shobyang <shoby.ctlno@gmail.com>
2023-05-18 18:12:30 +0800

    bg red

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
     <style>
         body{
-            color:blue;
+            color:red;
         }
     </style>
 </head>
